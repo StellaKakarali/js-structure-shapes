47e9549add99dff23680d4365ea81e3630ab67dd
Stergiani Kakarali <152175194+StellaKakarali@users.noreply.github.com>
2023-12-15 16:13:08 +0100

    creating colorful shapes with getRandonColor funtion

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ console.clear();
 
 const root = document.getElementById("root");
 
+function getRandomColor() {
+  return `hsl(${Math.floor(Math.random() * 360)} 80% 55%)`;
+}
+
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  circle.style.backgroundColor = getRandomColor();
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  square.style.backgroundColor = getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  pentagon.style.backgroundColor = getRandomColor();
 });
 
 root.append(circle, square, pentagon);
